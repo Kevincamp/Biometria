ea00fac1add669dde8deca627f3d09400401500b
Kevin Campuzano <kcampuzanocastillo@gmail.com>
2024-01-24 15:46:45 -0500

    update packages

---
+++ Networking/Sources/NetworkingPackage/NetworkingClient.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkingClient.swift
+//  Networking
+//
+//  Created by Kevin Campuzano on 1/24/24.
+//
+
+import Foundation
+open class NetworkingClient {
+    public static func helloWorld() {
+        print("Hello world from Networking")
+    }
+}
